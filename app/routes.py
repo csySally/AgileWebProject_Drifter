c0f584239ed903ed2b5328629fbda1975e1099fb
Lili Liu <78537049+LiliLiu09@users.noreply.github.com>
2024-04-27 23:34:31 +0800

    add flask_index.html and flask_login.html

--- app/routes.py
+++ app/routes.py
@@ -15,7 +15,7 @@ def index():
       'body': 'The Avengers movie was so cool!'
     }
   ]
-  return render_template('index.html', title='Home', user=user, posts=posts)
+  return render_template('flask_index.html', title='Home', user=user, posts=posts)
 
 @app.route('/login',methods=['GET', 'POST'])
 def login():
@@ -24,4 +24,4 @@ def login():
     flash('Login requested for user {}, remember_me={}'.format(
       form.username.data, form.remember_me.data))
     return redirect(url_for('index'))
-  return render_template('login.html', title='login', form=form)
+  return render_template('flask_login.html', title='login', form=form)
