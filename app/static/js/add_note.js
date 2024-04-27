6db39e2d1f89151119cc20ff050ca4dc221c3c07
Sally Chen <23687599@student.uwa.edu.au>
2024-04-27 20:23:43 +0800

    Link index with add_note.html

---
+++ app/static/js/add_note.js
@@ -0,0 +1,4 @@
+/* return to the index when click the btn-close button */
+document.querySelector(".btn-close").addEventListener("click", function () {
+  window.location.href = "../templates/index.html";
+});
