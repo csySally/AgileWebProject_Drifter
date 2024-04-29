42c1e12f79971c96de4f5fe07c305ccac9fc6447
Lili Liu <78537049+LiliLiu09@users.noreply.github.com>
2024-04-29 09:01:48 +0800

    add regitser function in forms.py and routes.py

--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,12 @@ from flask import render_template, redirect, url_for, flash
 from flask_login import current_user, login_user
 from flask_login import logout_user
 from flask_login import login_required
-from app import app
+from app import app, db
 from app.forms import LoginForm
 from app.models import User
 from flask import request
 from urllib.parse import urlparse
+from app.forms import RegistrationForm
 
 @app.route('/')
 @app.route('/index')
@@ -34,7 +35,7 @@ def login():
     if user is None or not user.check_password(form.password.data):
         flash('Invalid username or password')
         return redirect(url_for('login'))
-    login_user(user, remember=form.remember_me.data)
+    login_user(user)
     next_page = request.args.get('next')
     if not next_page or urlparse(next_page).netloc != '':
       next_page = url_for('index')
@@ -45,3 +46,18 @@ def login():
 def logout():
   logout_user() 
   return redirect(url_for('index')) 
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+  if current_user.is_authenticated:
+    return redirect(url_for('index'))
+  form = RegistrationForm()
+  if form.validate_on_submit():
+    user = User(username=form.username.data)
+    user.set_password(form.password.data)
+    db.session.add(user)
+    db.session.commit()
+    flash('Congratulations, you are now a registered user!')
+    return redirect(url_for('login'))
+  return render_template('flask_register.html', title='Register', form=form)
