9212f76f7f58e79a4e89e66c1ba0455b2a094007
Sally Chen <23687599@student.uwa.edu.au>
2024-04-29 09:51:35 +0800

    now can edit the labels: - after keypress enter, the label will be added - click the cross to remove the label - after sending the note will go back to index

--- app/static/js/add_note.js
+++ app/static/js/add_note.js
@@ -22,6 +22,9 @@ $("#noteInput").on("keyup", (event) => {
   $("#words").html(remaining);
 });
 
+/*  hide the label section when the page is loaded */
+$("#label-section").hide();
+
 /* alert when you type more than 100 words and cannot click 'next' button */
 document.querySelector("#btn-next").addEventListener("click", function () {
   /* need to wait for the cursor leaves the text area and get the count from '#words' */
@@ -30,6 +33,35 @@ document.querySelector("#btn-next").addEventListener("click", function () {
   if (wordCount < 0) {
     alert("You can only type up to 60 words.");
   } else {
-    window.location.href = "../templates/add_tag.html";
+    /* show the label section when the button is clicked */
+    $("#label-section").show();
+    $("#labelInput").keypress(function (e) {
+      if (e.which == 13) {
+        e.preventDefault();
+        var labelValue = $(this).val().trim();
+        if (labelValue && $("#label-list .label").length < 5) {
+          var newLabel = $(
+            '<div><p class="label"></p><button type="button" class="btn-close" id="close-label" aria-label="Close"></button></div>'
+          );
+          newLabel.find(".label").text(labelValue);
+          $("#label-list").append(newLabel);
+          $(this).val("");
+        }
+      }
+    });
+
+    /* when click the close button, remove the label */
+    $("#label-list").on("click", ".btn-close", function () {
+      $(this).parent().remove();
+    });
+
+    /* when the OK! button is clicked, send the labels to the server */
+    $("#btn-next2").click(function () {
+      // ...
+
+      // jump to index after the note is added
+      alert("You have successfully added a note!");
+      window.location.href = "../templates/index.html";
+    });
   }
 });
