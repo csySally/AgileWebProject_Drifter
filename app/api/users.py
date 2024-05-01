a6814710566c697fa15b5636b65891f0ac42e879
Lili Liu <78537049+LiliLiu09@users.noreply.github.com>
2024-05-01 20:19:39 +0800

    Add get user api

--- app/api/users.py
+++ app/api/users.py
@@ -1,6 +1,10 @@
 from app.api import bp
 from app.models import User
 from app import db
+import sqlalchemy as sa
+from flask import request
+from flask import url_for
+from app.api.errors import bad_request
 
 @bp.route('/users/<int:id>', methods=['GET'])
 def get_user(id):
@@ -8,19 +12,27 @@ def get_user(id):
 
 @bp.route('/users', methods=['GET'])
 def get_users():
-    pass
-
-@bp.route('/users/<int:id>/followers', methods=['GET'])
-def get_followers(id):
-    pass
-
-@bp.route('/users/<int:id>/following', methods=['GET'])
-def get_following(id):
-    pass
+    users_query = sa.select(User)
+    users = db.session.execute(users_query).scalars().all()
+    users_data = [user.to_dict() for user in users]
+    return users_data
 
 @bp.route('/users', methods=['POST'])
 def create_user():
-    pass
+    data = request.get_json()
+
+    if 'username' not in data or 'password' not in data:
+        return bad_request('must include username and password fields')
+      
+    if db.session.scalar(sa.select(User).where(User.username == data['username'])):
+        return bad_request('please use a different username')
+    user = User()
+    user.from_dict(data, new_user=True)
+    db.session.add(user)
+    db.session.commit()
+
+    return user.to_dict(), 201, {'Location': url_for('api.get_user',
+                                                     id=user.id)}
 
 @bp.route('/users/<int:id>', methods=['PUT'])
 def update_user(id):
