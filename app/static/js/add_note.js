ce021077f97610fc63d3d3da47d0a91a8d27860d
Quoka-PYX <127728293+Dave-114-P@users.noreply.github.com>
2024-05-02 15:41:01 +0800

    1. enabled note content check and label content check. 2. added a close btn in label popup

--- app/static/js/add_note.js
+++ app/static/js/add_note.js
@@ -1,18 +1,37 @@
-/* return to the index when click the btn-close button */
-document.querySelector(".btn-close").addEventListener("click", function () {
+/* return to the index when click the btn-close button */1
+document.querySelector("#close-note").addEventListener("click", function () {
   window.location.href = "../templates/index.html";
 });
-
+document.querySelector("#close-add-label").addEventListener("click", function () {
+  $("#label-section").hide();
+});
 /* limit the word count in the text area */
 $("#noteInput").focus();
+// max words count as 100 
+var wordMax = 100;
+// content inside text input area
+var post = "";
+
+function validateContent(po) {
+  po.replace(/[^a-zA-Z\u00C0-\u017F]/g, ''); // remove any punctuation
+  if (po.length == 0) {
+    return false;
+  }
+  return true;
+}
+function read_labels() {
+
+}
 
 $("#noteInput").on("keyup", (event) => {
-  let post = $(event.currentTarget).val();
+  
+  post = $(event.currentTarget).val();
+  console.log(post);
   let words = post.trim().split(/\s+/);
   let wordCount = words.length;
-  let remaining = 100 - wordCount;
+  let remaining = wordMax - wordCount;
   if (post.trim().length === 0) {
-    remaining = 100;
+    remaining = wordMax;
   }
   if (remaining <= 0) {
     $("#words").css("color", "red");
@@ -32,7 +51,14 @@ document.querySelector("#btn-next").addEventListener("click", function () {
   console.log(wordCount);
   if (wordCount < 0) {
     alert("You can only type up to 60 words.");
-  } else {
+  } 
+  else if (wordCount == wordMax){
+    alert("Please enter content before sending")
+  }
+  else if (!validateContent(post)){
+    alert("Please enter content worth sending");
+  }
+  else {
     /* show the label section when the button is clicked */
     $("#label-section").show();
     $("#labelInput").keypress(function (e) {
@@ -57,11 +83,23 @@ document.querySelector("#btn-next").addEventListener("click", function () {
 
     /* when the OK! button is clicked, send the labels to the server */
     $("#btn-next2").click(function () {
-      // ...
-
-      // jump to index after the note is added
-      alert("You have successfully added a note!");
-      window.location.href = "../templates/index.html";
+      // check the number of labels to send 
+      let childCount = $("#label-list").children().length;
+      console.log('num of lab'+childCount);
+      // no sending if no label were added
+      if (childCount == 0) {
+        alert("Please add at least one label");
+      }
+      else {
+        // jump to index after the note is added
+        let labelList = [];
+        $("#label-list").children().each(function() {
+          labelList.push($(this).find(".label").text());
+        });
+        console.log(labelList);
+        alert("You have successfully added a note!");
+        window.location.href = "../templates/index.html";
+      }
     });
   }
 });
