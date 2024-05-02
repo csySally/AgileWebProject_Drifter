6f43af3a19e658896b1b12b0d8633a9b3701321a
Lili Liu <78537049+LiliLiu09@users.noreply.github.com>
2024-05-02 23:24:27 +0800

    add table send reply labels in models

--- app/config.py
+++ app/config.py
@@ -1,7 +1,8 @@
 import os
+import secrets
 basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
-  SECRET_KEY = os.environ.get('SECRET_KEY') or 'am_the_best'
+  SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(16)
   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     
