946bbe11911a29b3f5e4bcba3c4d6c22fd841958
Sally Chen <23687599@student.uwa.edu.au>
2024-05-04 21:42:31 +1000

    static search for a note done

--- app/static/js/reply_note_entry.js
+++ app/static/js/reply_note_entry.js
@@ -1,3 +1,28 @@
-$(".btn-close").on("click", () => {
-  window.location.href = "../templates/index.html";
+$(document).ready(function () {
+  $(".btn-close").on("click", () => {
+    window.location.href = "../templates/index.html";
+  });
+
+  $("#search").on("click", () => {
+    $("#cover").css("display", "block");
+    $("#search-label-card").css("display", "block");
+  });
+
+  $("#search-input").on("keyup", () => {
+    const searchInput = $("#search-input").val();
+    if (searchInput.length > 0) {
+      $("#search-label-btn").prop("disabled", false);
+      $("#search-label-btn").on("click", () => {
+        window.location.href = "../templates/check_and_reply.html";
+      });
+      // after return key is pressed, can also trigger the click event
+      $("#search-input").on("keypress", (e) => {
+        if (e.which === 13) {
+          $("#search-label-btn").trigger("click");
+        }
+      });
+    } else {
+      $("#search-label-btn").prop("disabled", true);
+    }
+  });
 });
--- app/static/js/reply_random.js
+++ app/static/js/reply_random.js
@@ -17,12 +17,10 @@ $(document).ready(function () {
     window.location.href = "../templates/reply_note_entry.html";
   });
 
-  // 事件委托处理关闭按钮
   $(document).on("click", ".btn-close", function () {
-    $("#contentContainer").empty(); // 清空内容
+    $("#contentContainer").empty();
   });
 
-  // 处理查看另一条笔记的按钮
   $(document).on("click", "#check-next", function () {
     fetchRandomNote();
   });
@@ -41,9 +39,7 @@ $(document).ready(function () {
   }
 
   $(document).on("click", "#reply", function () {
-    // 隐藏查看笔记内容
     $("#reply-content").hide();
-    // 显示回复笔记内容
     $("#replyContainer").css("display", "block");
   });
 });
