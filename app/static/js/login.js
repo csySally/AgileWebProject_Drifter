c6e3bcf35d9f5c02d0be20e37951422bdc8baedc
Sally Chen <23687599@student.uwa.edu.au>
2024-05-09 17:15:54 +1000

    make login work

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -1,30 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-    const loginForm = document.getElementById('login-form');
-    // Listen on form submission events
-    loginForm.addEventListener('submit', function(event) {
-        // prevent form to submit
-        event.preventDefault();
-        // get username and password from
-        const usn = document.getElementById('username').value;
-        const pwd = document.getElementById('password').value;
-        console.log(usn, pwd);
-        // if (pwd){
-        //   return;
-        // }
-        $.ajax({
-          url: '/login',
-          type: 'POST',
-          contentType: 'application/json',
-          data: JSON.stringify({username: usn, password: pwd}),
-          success: function(response) {
-              // login successful
-              window.location.href = '/index';
-          },
-          error: function(xhr, status, error) {
-              // login failed and show error
-              alert(xhr.responseText);
-          }
-        })
+document.addEventListener("DOMContentLoaded", function () {
+  const loginForm = document.getElementById("login-form");
+  // Listen on form submission events
+  loginForm.addEventListener("submit", function (event) {
+    // prevent form to submit
+    event.preventDefault();
+    // get username and password from
+    const usn = document.getElementById("username").value;
+    const pwd = document.getElementById("password").value;
+    console.log(usn, pwd);
+    // if (pwd){
+    //   return;
+    // }
+    $.ajax({
+      url: "/login",
+      type: "POST",
+      contentType: "application/json",
+      data: JSON.stringify({ username: usn, password: pwd }),
+      success: function (response) {
+        // login successful
+        window.location.href = "/index";
+      },
+      error: function (xhr, status, error) {
+        // login failed and show error
+        alert(xhr.responseText);
+      },
     });
+  });
 });
