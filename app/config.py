b64af8f5b4000e8a83d0ffa2a58823a9b620ea7f
Lili Liu <78537049+LiliLiu09@users.noreply.github.com>
2024-05-11 11:58:34 +0800

    add testingConfig, DevelopmentConfig

--- app/config.py
+++ app/config.py
@@ -2,8 +2,17 @@ import os
 import secrets
 basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
-  SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(16)
-  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
-    
-  SQLALCHEMY_TRACK_MODIFICATIONS = False
+      SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(16)
+      SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+            'sqlite:///' + os.path.join(basedir, 'app.db')
+      
+      SQLALCHEMY_TRACK_MODIFICATIONS = False
+  
+class DevelopmentConfig(Config):
+      SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, 'test.db')
+
+  
+class TestingConfig(Config):
+      SQLALCHEMY_DATABASE_URI = "sqlite:///:memory"
+      TESTING = True
+  
