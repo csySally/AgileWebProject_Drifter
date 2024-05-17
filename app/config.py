6326a01b1b3d2ebb05b1cdcbda6db3fccb3bc3ae
Lili Liu <78537049+LiliLiu09@users.noreply.github.com>
2024-05-17 15:18:40 +0800

    add test and dev config into config

--- app/config.py
+++ app/config.py
@@ -1,18 +1,32 @@
 import os
 import secrets
-basedir = os.path.abspath(os.path.dirname(__file__))
-class Config(object):
-      SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(16)
-      SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-            'sqlite:///' + os.path.join(basedir, 'app.db')
-      
-      SQLALCHEMY_TRACK_MODIFICATIONS = False
-  
-class DevelopmentConfig(Config):
-      SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, 'test.db')
 
-  
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(16)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'dev.db')
+    DEBUG = True
+
 class TestingConfig(Config):
-      SQLALCHEMY_DATABASE_URI = "sqlite:///:memory"
-      TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    TESTING = True
+    WTF_CSRF_ENABLED = False
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
+    DEBUG = False
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
+
   
