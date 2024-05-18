e631e0eba9eb2cb2f479b076600a0a1d99a22340
Lili Liu <78537049+LiliLiu09@users.noreply.github.com>
2024-05-18 20:27:52 +0800

    move random_other_note into api

--- app/api/users.py
+++ app/api/users.py
@@ -2,18 +2,21 @@ from app.api import bp
 from app.models import User, Send, Reply
 from app import db
 import sqlalchemy as sa
-from flask import request
+from flask import request,jsonify, url_for
 from flask import url_for
 from app.api.errors import bad_request
+import random
+from flask_login import login_required, current_user
+
 
 @bp.route('/users/<int:id>', methods=['GET'])
 def get_user_byID(id):
-    #Retrieve a user by their ID.
+    # Retrieve a user by their ID.
     return db.get_or_404(User, id).to_dict()
 
 @bp.route('/users', methods=['GET'])
 def get_users():
-    #Retrieve all users.
+    # Retrieve all users.
     users_query = sa.select(User)
     users = db.session.execute(users_query).scalars().all()
     users_data = [user.to_dict() for user in users]
@@ -21,7 +24,7 @@ def get_users():
 
 @bp.route('/users', methods=['POST'])
 def create_user():
-    #Create a new user.
+    # Create a new user.
     data = request.get_json()
 
     if 'username' not in data or 'password' not in data:
@@ -34,8 +37,7 @@ def create_user():
     db.session.add(user)
     db.session.commit()
 
-    return user.to_dict(), 201, {'Location': url_for('api.get_user',
-                                                     id=user.id)}
+    return user.to_dict(), 201, {'Location': url_for('api.get_user_byID', id=user.id)}
 
 @bp.route('/users/<int:id>', methods=['PUT'])
 def update_user(id):
@@ -43,7 +45,7 @@ def update_user(id):
 
 @bp.route('/users/<int:id>/send', methods=['GET'])
 def get_user_send(id):
-    #Retrieve all messages (sends) sent by a user.
+    # Retrieve all messages (sends) sent by a user.
     user = User.query.get(id)
     if not user:
         return bad_request('User not found')
@@ -52,10 +54,9 @@ def get_user_send(id):
     sends_data = [send.to_dict() for send in sends]
     return sends_data
 
-
 @bp.route('/users/<int:id>/send', methods=['POST'])
 def create_send(id):
-    #Create a new message (send) for a user.
+    # Create a new message (send) for a user.
     user = User.query.get(id)
     if not user:
         return bad_request('can not find user with id')
@@ -69,25 +70,22 @@ def create_send(id):
     db.session.add(send)
     db.session.commit()
 
-    return user.to_dict(), 201, {'Location': url_for('api.get_user_send',
-                                                     id=user.id)}
-    
+    return send.to_dict(), 201, {'Location': url_for('api.get_user_send', id=user.id)}
     
 @bp.route('/users/<int:id>/reply', methods=['GET'])
 def get_user_reply(id):
-    #Retrieve all replies made by a user.
+    # Retrieve all replies made by a user.
     user = User.query.get(id)
     if not user:
         return bad_request('User not found')
     reply_query = sa.select(Reply).where(Reply.userId == user.id)
-    replys = db.session.execute(reply_query).scalars().all()
-    reply_data = [reply.to_dict() for reply in replys]
+    replies = db.session.execute(reply_query).scalars().all()
+    reply_data = [reply.to_dict() for reply in replies]
     return reply_data
-
 
 @bp.route('/users/<int:user_id>/reply', methods=['POST'])
 def create_reply(user_id):
-    #Create a new reply to a message (send) for a user.
+    # Create a new reply to a message (send) for a user.
     user = User.query.get(user_id)
     
     if not user:
@@ -113,3 +111,34 @@ def create_reply(user_id):
     db.session.commit()
 
     return reply.to_dict(), 201, {'Location': url_for('api.get_user_reply', id=user.id)}
+
+
+@bp.route('/random_other_note', methods=['GET'])
+@login_required
+def random_other_note():
+    # Returns a random note from other users as JSON. If no notes are available, returns a 404 status code.
+    all_other_notes = Send.query.filter(Send.userId != current_user.id).all()
+    if not all_other_notes:
+        return jsonify({"error": "No other notes available"}), 404
+
+    random_note = random.choice(all_other_notes)
+    if random_note.anonymous:
+        avatar_url = url_for("static", filename="images/default-avatar.png")
+    else:
+        avatar_url = url_for(
+            "static",
+            filename=(
+                random_note.author.avatar_path
+                if random_note.author.avatar_path
+                else "images/default-avatar.png"
+            ),
+        )
+    return jsonify(
+        {
+            "id": random_note.id,
+            "body": random_note.body,
+            "author": random_note.author.username,
+            "anonymous": random_note.anonymous,
+            "avatar_url": avatar_url,
+        }
+    )
